5f3f3efe036c30067e36e122d8046e48d498cada
Sravika88 <161863954+Sravika88@users.noreply.github.com>
2024-12-03 15:02:57 -0500

    Update Puzzleboard.py

--- Puzzleboard.py
+++ Puzzleboard.py
@@ -1,17 +1,9 @@
 class PuzzleBoard:
     def __init__(self, initial_state):
-        """
-        Initialize the puzzle board with the given state.
-        :param initial_state: List of lists representing the 8-puzzle board.
-        """
         self.state = initial_state
         self.empty_tile = self.find_empty_tile()
 
     def find_empty_tile(self):
-        """
-        Find the position of the empty tile (represented as 0).
-        :return: Tuple of (row, col) for the empty tile.
-        """
         for row in range(len(self.state)):
             for col in range(len(self.state[row])):
                 if self.state[row][col] == 0:
@@ -19,10 +11,27 @@ class PuzzleBoard:
         return None
 
     def format_state(self):
-        """
-        Generate a unique string representation of the current state.
-        :return: String representation of the state.
-        """
-        return ''.join(str(num) for row in self.state for num in row)
+        return tuple(tuple(row) for row in self.state)
 
-    def is_goal_sta
+    def is_goal_state(self, goal_state):
+        return self.state == goal_state
+
+    def generate_moves(self):
+        row, col = self.empty_tile
+        moves = []
+        potential_moves = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        for move in potential_moves:
+            new_row = row + move[0]
+            new_col = col + move[1]
+            if 0 <= new_row < len(self.state) and 0 <= new_col < len(self.state[0]):
+                new_state = [row[:] for row in self.state]
+                new_state[row][col], new_state[new_row][new_col] = new_state[new_row][new_col], new_state[row][col]
+                moves.append(PuzzleBoard(new_state))
+        return moves
+
+if __name__ == "__main__":
+    board = PuzzleBoard([[1, 2, 3], [4, 5, 0], [6, 7, 8]])
+    print("Initial State:")
+    print(board.state)
+    print("Possible Moves:")
+    print([move.state for move in board.generate_moves()])

7d8b0dd0fcb172cb32cbbd8718e1e5a74996b820
Sravika88 <161863954+Sravika88@users.noreply.github.com>
2024-12-06 17:54:41 -0500

    Update DFS.py

--- DFS.py
+++ DFS.py
@@ -1,40 +1,49 @@
-from PuzzleBoard import PuzzleBoard
+from Assignment.Puzzleboard import PuzzleBoard
 
 def dfs(initial_state, goal_state):
-    print("Debug: Starting DFS execution.")
-    stack = [initial_state]  # Use a stack for DFS
-    visited = set()  # To track visited states
+    """
+    Perform Depth-First Search to find the solution to the 8-puzzle problem.
+    :param initial_state: List of lists representing the initial puzzle state.
+    :param goal_state: List of lists representing the goal puzzle state.
+    :return: Solution path as a list of states, or None if no solution is found.
+    """
+    stack = [(PuzzleBoard(initial_state), [])]
+    visited = set()
 
     while stack:
-        current_board = stack.pop()  # Get the last added state
-        print(f"Exploring state:\n{current_board.state}")
-        visited.add(current_board.format_state())  # Mark as visited
+        current_board, path = stack.pop()
 
-        # Check if the current state is the goal
         if current_board.is_goal_state(goal_state):
-            print("Solution found!")
-            return current_board.state
+            return path + [current_board.state]
 
-        # Generate possible moves and add to the stack if not visited
+        if current_board.format_state() in visited:
+            continue
+
+        visited.add(current_board.format_state())
+
         for move in current_board.generate_moves():
-            if move.format_state() not in visited:
-                stack.append(move)
+            stack.append((move, path + [current_board.state]))
 
-    print("No solution found!")
-    return None
-
+    return None  # Return None if no solution is found
 
 if __name__ == "__main__":
-    # Define the initial and goal states
-    initial_state = PuzzleBoard([[1, 2, 3], [4, 5, 0], [6, 7, 8]])
-    goal_state = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+    initial_state = [
+        [1, 2, 3],
+        [4, 0, 5],
+        [6, 7, 8],
+    ]
+    goal_state = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 0],
+    ]
 
-    # Run the DFS algorithm
     solution = dfs(initial_state, goal_state)
-
-    # Print the solution steps if found
     if solution:
-        print("Solution path:")
-        print(solution)
+        print("Solution found!")
+        for step in solution:
+            for row in step:
+                print(row)
+            print()
     else:
-        print("No solution path could be found.")
+        print("No solution exists.")
